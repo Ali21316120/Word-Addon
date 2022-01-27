10e6ea533f422063284460df85f55dedb24308b2
Ali <upwork21316120@gmail.com>
2022-01-27 17:10:38 +1000

    first commit

---
+++ WordAddIn1/ThisAddIn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Office.Core;
+using Newtonsoft.Json;
+
+namespace WordAddIn1
+{
+    public partial class ThisAddIn
+    {
+        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            var blabla = JsonConvert.SerializeObject("just a teste to prove that external dlls are working good");
+
+            return new MyRibbon();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with
+        /// the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion VSTO generated code
+    }
+}
---
+++ WordAddIn1/conv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using WL;
+
+namespace WordAddIn1
+{
+    public partial class conv : Form
+    {
+        private WDHelper gWDHelper;		//Classe de fonction WDHelper.DLL généré depuis WinDev.
+
+        public conv()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gWDHelper = new WDHelper();
+        }
+    }
+}
